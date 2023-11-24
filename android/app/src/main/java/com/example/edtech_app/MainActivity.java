543e3ea8e95fb4f5304a1efe35de203f260225d9
EHTarek <tariqul.191002037@green.ac.bd>
2023-11-24 23:52:28 +0600

    [EdTech App]
    - project initial

---
+++ android/app/src/main/java/com/example/edtech_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.edtech_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'dart:io';
+
+import 'package:edtech_app/app_config.dart';
+import 'package:edtech_app/constants/app_observer.dart';
+import 'package:edtech_app/constants/navigation/route_generator.dart';
+import 'package:edtech_app/constants/navigation/routes.dart';
+import 'package:edtech_app/constants/theme/app_theme.dart';
+import 'package:edtech_app/features/dashboard/domain/use_cases/get_dashboard_products_category.dart';
+import 'package:edtech_app/features/dashboard/presentation/business_logic/dashboard_products_category_bloc/dashboard_products_category_bloc.dart';
+import 'package:edtech_app/firebase_options.dart';
+import 'package:edtech_app/injection_container.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+    // [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight],
+  );
+
+  if (Platform.isAndroid) {
+    HttpOverrides.global = MyHttpOverrides();
+  }
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  Bloc.observer = AppObserver();
+  await init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => DashboardProductsCategoryBloc(
+            getDashboardProductsCategory: sl<GetDashboardProductsCategory>(),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: appName,
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: AppTheme.lightColorScheme,
+        ),
+        darkTheme: ThemeData(
+          useMaterial3: true,
+          colorScheme: AppTheme.darkColorScheme,
+        ),
+        themeMode: ThemeMode.system,
+
+        /// Navigation
+        initialRoute: Routes.kRoot,
+        onGenerateRoute: RouteGenerator.generateRoute,
+      ),
+    );
+  }
+}

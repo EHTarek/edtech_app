5073d33fa9a995b9b37a59d9fbc540d261a0ffa7
EHTarek <tariqul.191002037@green.ac.bd>
2023-11-25 11:33:50 +0600

    [EdTech App]
    - added auth login screen ui and validation
    - added auth signup screen ui and validation

--- lib/features/auth/presentation/screens/auth_login_screen.dart
+++ lib/features/auth/presentation/screens/auth_login_screen.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
+
 import 'package:edtech_app/constants/navigation/routes.dart';
-import 'package:edtech_app/features/auth/presentation/screens/views/auth_title_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AuthLoginScreen extends StatefulWidget {
   const AuthLoginScreen({super.key});
@@ -12,8 +14,13 @@ class AuthLoginScreen extends StatefulWidget {
 
 class _AuthLoginScreenState extends State<AuthLoginScreen> {
   final _auth = FirebaseAuth.instance;
-  late String email;
-  late String password;
+
+  // late String email;
+  // late String password;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
@@ -22,78 +29,173 @@ class _AuthLoginScreenState extends State<AuthLoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      /*appBar: AppBar(
         title: const AuthTitleView(title: 'Login'),
         centerTitle: true,
-      ),
-      body: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 100.0,
-                  child: Image.asset('assets/images/app_logo.png'),
-                ),
-                const SizedBox(height: 48.0),
-                TextField(
-                  textAlign: TextAlign.center,
-                  keyboardType: TextInputType.emailAddress,
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  decoration: const InputDecoration(
-                    hintText: 'Enter your email',
+      ),*/
+      body: PopScope(
+        canPop: false,
+        onPopInvoked: (didPop) {
+          showDialog<void>(
+            context: context,
+            barrierDismissible: true,
+            builder: (BuildContext dialogContext) {
+              return AlertDialog(
+                title: const Text('Want to exit now?'),
+                content: const Text('dialogBody'),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('Cancel'),
+                    onPressed: () {
+                      Navigator.of(dialogContext).pop();
+                    },
                   ),
-                ),
-                const SizedBox(height: 8.0),
-                TextField(
-                  textAlign: TextAlign.center,
-                  obscureText: true,
-                  onChanged: (value) {
-                    password = value;
-                  },
-                  decoration: const InputDecoration(
-                    hintText: 'Enter your password',
+                  TextButton(
+                    child: const Text('Confirm'),
+                    onPressed: () {
+                      if (Platform.isAndroid) {
+                        SystemNavigator.pop();
+                      } else if (Platform.isIOS) {
+                        exit(0);
+                      }
+                    },
                   ),
-                ),
-                const SizedBox(height: 24.0),
-                OutlinedButton(
-                  onPressed: () async {
-                    Navigator.pushNamed(context, Routes.kAuthSignupScreen);
+                ],
+              );
+            },
+          );
+        },
+        child: CustomScrollView(
+          physics: const BouncingScrollPhysics(),
+          slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 120.0),
+                    Text(
+                      'Login',
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
+                    const SizedBox(height: 24.0),
+                    Text(
+                      'Hello there, haven’t we seen you before?',
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                    const SizedBox(height: 48.0),
+                    Form(
+                      key: loginFormKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextFormField(
+                            textAlign: TextAlign.center,
+                            keyboardType: TextInputType.emailAddress,
+                            controller: emailController,
+                            onChanged: (value) {
+                              emailController.text = value;
+                            },
+                            validator: (value) => 'Enter a valid email address',
+                            autovalidateMode: AutovalidateMode.onUserInteraction,
+                            decoration: const InputDecoration(
+                              hintText: 'Enter your email',
+                              labelText: 'Enter your email',
+                              hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                              border: OutlineInputBorder(),
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: 4,
+                                horizontal: 8,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 16.0),
+                          TextFormField(
+                            textAlign: TextAlign.center,
+                            controller: passwordController,
+                            obscureText: true,
+                            onChanged: (value) {
+                              passwordController.text = value;
+                            },
+                            validator: (value) => 'Enter a valid password',
+                            autovalidateMode: AutovalidateMode.onUserInteraction,
+                            decoration: const InputDecoration(
+                              hintText: 'Enter your password',
+                              labelText: 'Enter your password',
+                              hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                              border: OutlineInputBorder(),
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: 4,
+                                horizontal: 8,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 24.0),
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (loginFormKey.currentState != null &&
+                            loginFormKey.currentState!.validate()) {
+                          /*try {
+                          final user = await _auth.signInWithEmailAndPassword(
+                              email: email, password: password);
+                          if(user != null){
+                            //Navigate to dashboard
+                          }
+                        }catch(e){}*/
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                      ),
+                      child: Text(
+                        'LOGIN NOW',
+                        style: TextStyle(
+                            color: Theme.of(context).colorScheme.onPrimary),
+                      ),
+                    ),
+                    const SizedBox(height: 24.0),
+                    Row(
+                      children: [
+                        const Text('New here?'),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(
+                                context, Routes.kAuthSignupScreen);
+                          },
+                          child: const Text('Sign Up'),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 24.0),
+                    OutlinedButton(
+                      onPressed: () async {
+                        Navigator.pushNamed(context, Routes.kDashboardScreen);
 
-                    /*try {
-                      final user = await _auth.signInWithEmailAndPassword(
-                          email: email, password: password);
-                      if(user != null){
-                        //Navigate to dashboard
-                      }
-                    }catch(e){}*/
-                  },
-                  child: const Text('Register'),
+                        /*try {
+                          final user = await _auth.signInWithEmailAndPassword(
+                              email: email, password: password);
+                          if(user != null){
+                            //Navigate to dashboard
+                          }
+                        }catch(e){}*/
+                      },
+                      child: const Text('Dashboard'),
+                    ),
+                  ],
                 ),
-                OutlinedButton(
-                  onPressed: () async {
-                    Navigator.pushNamed(context, Routes.kDashboardScreen);
-
-                    /*try {
-                      final user = await _auth.signInWithEmailAndPassword(
-                          email: email, password: password);
-                      if(user != null){
-                        //Navigate to dashboard
-                      }
-                    }catch(e){}*/
-                  },
-                  child: const Text('Dashboard'),
-                ),
-              ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
-
--- lib/features/auth/presentation/screens/auth_signup_screen.dart
+++ lib/features/auth/presentation/screens/auth_signup_screen.dart
@@ -1,4 +1,3 @@
-import 'package:edtech_app/features/auth/presentation/screens/views/auth_title_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +12,9 @@ class _AuthSignupScreenState extends State<AuthSignupScreen> {
   final _auth = FirebaseAuth.instance;
   late String email;
   late String password;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  GlobalKey<FormState> signupFormKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -22,58 +24,124 @@ class _AuthSignupScreenState extends State<AuthSignupScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      /*appBar: AppBar(
         title: const AuthTitleView(title: 'Signup'),
         centerTitle: true,
-      ),
+      ),*/
       body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
         slivers: [
           SliverToBoxAdapter(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 100.0,
-                  child: Image.asset('assets/images/app_logo.png'),
-                ),
-                const SizedBox(height: 48.0),
-                TextField(
-                  textAlign: TextAlign.center,
-                  keyboardType: TextInputType.emailAddress,
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  decoration: const InputDecoration(
-                    hintText: 'Enter your email',
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const SizedBox(height: 120.0),
+                  Text(
+                    'Sign Up',
+                    style: Theme.of(context).textTheme.headlineLarge,
                   ),
-                ),
-                const SizedBox(height: 8.0),
-                TextField(
-                  textAlign: TextAlign.center,
-                  obscureText: true,
-                  onChanged: (value) {
-                    password = value;
-                  },
-                  decoration: const InputDecoration(
-                    hintText: 'Enter your password',
+                  const SizedBox(height: 24.0),
+                  Row(
+                    children: [
+                      Text(
+                        'Already have an account?',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text(
+                          'Sign In',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(height: 24.0),
-                OutlinedButton(
-                  onPressed: () async {
-                    /*try {
+                  const SizedBox(height: 48.0),
+                  Form(
+                    key: signupFormKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
+                          textAlign: TextAlign.center,
+                          keyboardType: TextInputType.emailAddress,
+                          controller: emailController,
+                          onChanged: (value) {
+                            emailController.text = value;
+                          },
+                          validator: (value) => 'Enter a valid email address',
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          decoration: const InputDecoration(
+                            hintText: 'Enter your email',
+                            labelText: 'Enter your email',
+                            hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 4,
+                              horizontal: 8,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 16.0),
+                        TextFormField(
+                          textAlign: TextAlign.center,
+                          controller: passwordController,
+                          obscureText: true,
+                          onChanged: (value) {
+                            passwordController.text = value;
+                          },
+                          validator: (value) => 'Enter a valid password',
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          decoration: const InputDecoration(
+                            hintText: 'Enter your password',
+                            labelText: 'Enter your password',
+                            hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 4,
+                              horizontal: 8,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 24.0),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (signupFormKey.currentState != null &&
+                          signupFormKey.currentState!.validate()) {
+                        /*try {
                       final newUser =
                           await _auth.createUserWithEmailAndPassword(
                               email: email, password: password);
                       if (newUser != null) {
                         //Navigate to Dashboard
+                        Navigator.pop(context);
                       }
                     } catch (e) {}*/
-                  },
-                  child: const Text('Signup'),
-                ),
-              ],
+                        Navigator.pop(context);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                    ),
+                    child: Text(
+                      'REGISTER NOW',
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.onPrimary),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
